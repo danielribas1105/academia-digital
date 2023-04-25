2fa48dfd9ea69b3f5295fff25c92b12b5e1cf616
Daniel <danielribas1105@gmail.com>
2023-04-24 23:16:02 -0300

    annotations to db

--- src/main/java/drcode/academy/model/Matricula.java
+++ src/main/java/drcode/academy/model/Matricula.java
@@ -1,10 +1,24 @@
 package drcode.academy.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_matricula")
 public class Matricula {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "aluno_id")
     private Aluno aluno;
     private LocalDateTime dataMatricula = LocalDateTime.now();
 }
